ffaa34fdb796d17378be06b9bc444850bb9650b0
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-04-17 22:39:02 +0000

    draft crm

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,47 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS industries;
+
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  address TEXT,
+  phone_number INTEGER,
+  company_id INTEGER
+);
+
+CREATE TABLE activities (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    date INTEGER,
+    time INTEGER,
+    notes TEXT,
+    contact_id INTEGER
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company TEXT
+);
+
+CREATE TABLE salespersons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    activities_id INTEGER
+);
+
+CREATE TABLE industries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    industry TEXT,
+);
